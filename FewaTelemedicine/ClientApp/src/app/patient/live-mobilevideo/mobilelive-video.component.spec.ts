f389cf2ae0b8e832729ab5b77a7f2b713ee6b220
Dhanu-git20 <dhanashrimajgaonkar@gmail.com>
2021-05-20 18:19:42 +0530

    fixed android compatibility for patient live-video

--- FewaTelemedicine/ClientApp/src/app/patient/live-mobilevideo/mobilelive-video.component.spec.ts
+++ FewaTelemedicine/ClientApp/src/app/patient/live-mobilevideo/mobilelive-video.component.spec.ts
@@ -1,20 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MobileLiveVideoComponent } from './mobilelive-video.component';
 
-describe('LiveVideoComponent', () => {
-  let component: MobileLiveVideoComponent;
-  let fixture: ComponentFixture<MobileLiveVideoComponent>;
+describe('MobileLiveVideoComponent ', () => {
+  let component: MobileLiveVideoComponent ;
+  let fixture: ComponentFixture<MobileLiveVideoComponent >;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MobileLiveVideoComponent ]
+      declarations: [ MobileLiveVideoComponent  ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MobileLiveVideoComponent);
+    fixture = TestBed.createComponent(MobileLiveVideoComponent );
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
