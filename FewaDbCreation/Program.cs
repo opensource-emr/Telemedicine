af60dfe2f6035176f03b4f89abfe14c653fd40c8
Swapnil <smpatil1010@gmail.com>
2021-06-04 10:38:42 +0530

    added new project to run migration on published dll

---
+++ FewaDbCreation/Program.cs
@@ -0,0 +1,38 @@
+﻿using FewaTelemedicine.Domain;
+
+using System;
+using Microsoft.Extensions.Configuration;
+
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FewaDbCreation
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            Console.Write("Are you sure you want to run database migration? [y/n] ");
+            string option = Console.ReadLine();
+
+            if (option == "y")
+            {
+               // Console.WriteLine("yes");
+               // string bilConString = ConfigurationManager.ConnectionStrings["fewadb"].ConnectionString;
+                IDesignTimeDbContextFactory<FewaDbContext> factory = new FewaContextFactory();
+                var ctx = factory.CreateDbContext(new string[0]);
+                ctx.Database.Migrate();
+                ctx.Dispose();
+                Console.WriteLine("You have successfully migrated database");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+    }
+}
